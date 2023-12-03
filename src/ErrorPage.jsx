47ceaeccf08acb6e1019686cbb0a29627d0969de
Becca Jonas <beccajonas3@gmail.com>
2023-12-03 17:28:43 -0500

    add error page content

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,7 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import NavBar from './NavBar'; // Replace with the actual path to your NavBar component
+
 function ErrorPage() {
     return (
-        <div></div>
-    )
+        <div>
+            <NavBar />
+            <div style={{ textAlign: 'center', padding: '20px' }}>
+                <h1>Oops! Something went wrong.</h1>
+                <Link to="/">Go back to the home page</Link>
+            </div>
+        </div>
+    );
 }
 
-export default ErrorPage
+export default ErrorPage;
