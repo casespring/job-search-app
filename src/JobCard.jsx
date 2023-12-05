fbe34863cb3b1c00ac194fc5ca8e84dba5df3c7e
Marco Upia <markremembers@gmail.com>
2023-12-05 12:49:31 -0500

    changing bootstrap to regular react

--- src/JobCard.jsx
+++ src/JobCard.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
-import './JobCard.css';
+import { useState } from 'react';
 
-function JobCard({ jobs }) {
+function JobCard({ jobs, editMode }) {
   const [addFavorite, setAddFavorite] = useState(jobs.favorite);
   const [currentStatus, setCurrentStatus] = useState(jobs.status);
   const [notes, setNotes] = useState(jobs.notes);
@@ -10,77 +9,51 @@ function JobCard({ jobs }) {
 
   function handleStatusSelect(selected) {
     setCurrentStatus(selected);
-    updateJobData({ status: selected });
+    // Replace the fetch call with your actual API endpoint
+    console.log(`Status updated: ${selected}`);
   }
 
   function handleFavoritedClick() {
     setAddFavorite(!addFavorite);
-    updateJobData({ favorite: !addFavorite });
+    // Replace the fetch call with your actual API endpoint
+    console.log(`Favorite status updated: ${!addFavorite}`);
   }
 
   function handleNotesChange(e) {
     setNotes(e.target.value);
-    updateJobData({ notes: e.target.value });
-  }
-
-  function updateJobData(data) {
-    fetch(`http://localhost:3000/jobs/${jobs.id}`, {
-      method: 'PATCH',
-      headers: { 'content-type': 'Application/json' },
-      body: JSON.stringify(data),
-    })
-      .then((r) => r.json())
-      .then((updatedData) => {
-        setNotes(updatedData.notes);
-        // Add additional state updates as needed
-      });
+    // Replace the fetch call with your actual API endpoint
+    console.log(`Notes updated: ${e.target.value}`);
   }
 
   return (
-    <div className="job-card-container">
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">{jobs.jobTitle}</h5>
-          <p className="card-text">
-            <strong>Company:</strong> {jobs.company}
-          </p>
-          <p className="card-text">
-            <strong>Work Location:</strong> {jobs.workLocation}
-          </p>
-          <p className="card-text">
-            <strong>Date Applied:</strong> {jobs.dateApplied}
-          </p>
-          <p className="card-text">
-            <strong>Status:</strong>{' '}
-            <select
-              value={currentStatus}
-              onChange={(e) => handleStatusSelect(e.target.value)}
-            >
-              <option value="Applied 💼">Applied 💼</option>
-              <option value="Interview scheduled 🗓">Interview scheduled 🗓</option>
-              <option value="Interview complete ✅">Interview complete ✅</option>
-              <option value="Rejected ❌">Rejected ❌</option>
-            </select>
-          </p>
-          <p className="card-text">
-            <strong>Notes:</strong>
-          </p>
-          <textarea
-            rows={2}
-            value={notes}
-            onChange={handleNotesChange}
-          />
-          <a href={jobs.jobDescription} className="card-link">
-            Link to job description
-          </a>
-          <br />
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={handleFavoritedClick}
-          >
-            {addFavorite ? 'Remove from favorite' : 'Add to favorite'}
-          </button>
+    <div style={{ display: 'grid', gap: '20px', gridTemplateColumns: 'repeat(auto-fit, minmax(18rem, 1fr))', padding: '20px' }}>
+      <div>
+        <div style={{ width: '18rem', border: '2px solid #ccc', borderRadius: '10px', padding: '20px' }}>
+          <div>
+            <h5>{jobs.jobTitle}</h5>
+            <div><strong>Company:</strong> {jobs.company}</div>
+            <div><strong>Work Location:</strong> {jobs.workLocation}</div>
+            <div><strong>Date Applied:</strong> {jobs.dateApplied}</div>
+            <div><strong>Status:</strong> {editMode ?
+              <select value={currentStatus} onChange={(e) => handleStatusSelect(e.target.value)}>
+                <option value="Applied 💼">Applied 💼</option>
+                <option value="Interview scheduled 🗓">Interview scheduled 🗓</option>
+                <option value="Interview complete ✅">Interview complete ✅</option>
+                <option value="Rejected ❌">Rejected ❌</option>
+              </select> : jobs.status}
+            </div>
+            <div>
+              <strong>Notes:</strong> {editMode ?
+                <textarea
+                  rows={2}
+                  value={notes}
+                  onChange={handleNotesChange}
+                /> : jobs.notes}
+            </div>
+            <div><a href={jobs.jobDescription}>Link to job description</a></div>
+            <br />
+            <button onClick={handleFavoritedClick}>{addFavorite ? "⭐" : "☆"}</button>
+          </div>
         </div>
       </div>
     </div>
