17140c813285ec2af8e3f90f6e614e8fa1ae33e6
Becca Jonas <beccajonas3@gmail.com>
2023-12-06 15:09:33 -0500

    filter by favorites

--- src/JobTable.jsx
+++ src/JobTable.jsx
@@ -4,38 +4,42 @@
   
   function JobTable() {
     const [jobs, setJobs] = useState([]);
-    const [sortStatusOrder, setSortStatusOrder] = useState(null);
-    const [sortStatusDirection, setSortStatusDirection] = useState('asc');
-  
+
+    const [sortFavoriteOrder, setSortFavoriteOrder] = useState(null)
+    const [sortFavoriteDirection, setSortFavoriteDirection] = useState('asc')
+
     useEffect(() => {
       fetch(`http://localhost:3000/jobs`)
         .then((r) => r.json())
         .then((data) => setJobs(data));
     }, []);
   
-    const handleDeleteCallback = (deletedJobId) => {
+    function handleDelete(deletedJobId) {
       setJobs((prevJobs) => prevJobs.filter((job) => job.id !== deletedJobId));
-    };
-  
-    const handleStatusHeaderClick = () => {
-      setSortStatusDirection(
-        sortStatusOrder === 'status' && sortStatusDirection === 'asc' ? 'desc' : 'asc'
-      );
-      setSortStatusOrder('status');
-    };
-  
-    const statusOrder = ['Applied 💼', 'Interview scheduled 🗓', 'Interview complete ✅', 'Rejected ❌'];
-  
-    const sortedJobs = [...jobs];
-    if (sortStatusOrder === 'status') {
-      sortedJobs.sort((a, b) => {
-        const order = sortStatusDirection === 'asc' ? 1 : -1;
-        return order * (statusOrder.indexOf(a.status) - statusOrder.indexOf(b.status));
-      });
     }
   
+    function handleFavoriteHeaderClick() {
+      setSortFavoriteDirection(
+          sortFavoriteOrder === 'favorite' && sortFavoriteDirection === 'asc' ? 'desc' : 'asc'
+        )
+      setSortFavoriteOrder('favorite')
+      console.log(sortFavoriteOrder);
+      console.log(sortFavoriteDirection);
+    }
+
+    const favoriteOrder = [true, false]
+  
+    const sortedJobs = [...jobs];
+    
+    if (sortFavoriteOrder === 'favorite') {
+      sortedJobs.sort((a, b) => {
+        const order = sortFavoriteDirection === 'asc' ? 1 : -1;
+        return order * (favoriteOrder.indexOf(a.favorite) - favoriteOrder.indexOf(b.favorite));
+      });
+    } 
+    
     const displayJobs = sortedJobs.map((job) => (
-      <JobTableRow onDelete={handleDeleteCallback} job={job} key={job.id} />
+      <JobTableRow onDelete={handleDelete} job={job} key={job.id} />
     ));
 
   return (
@@ -46,10 +50,10 @@
             <th scope="col">Title</th>
             <th scope="col">Company</th>
             <th scope="col">Work Location</th>
-            <th scope="col" onClick={handleStatusHeaderClick}>Status</th>
+            <th scope="col">Status</th>
             <th scope="col">Date Applied</th>
             <th scope="col">Notes</th>
-            <th scope="col">Favorite</th>
+            <th scope="col" onClick={handleFavoriteHeaderClick}>Favorite</th>
             <th scope="col">Edit</th>
           </tr>
         </thead>
